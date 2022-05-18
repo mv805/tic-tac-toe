9df1248e8ccd60a278c6a5313c67c752cb9521de
Matt Villa <matthewvilla805@gmail.com>
2022-05-18 16:19:53 -0700

    Added basic player factory

---
+++ main.js
@@ -0,0 +1,10 @@
+const player = (playerNumber, marker) => {
+    return {playerNumber, marker};
+}
+
+const gameBoard = (() => {
+
+})();
+
+const player1 = player(1, 'X');
+const player2 = player(2, 'O');
